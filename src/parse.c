90e351809f0939f4c60911dd0546a3cb7b04fe28
hitchhooker <tommi@niemi.lol>
2025-07-06 06:02:09 +0700

    improve perf of binary search

--- src/parse.c
+++ src/parse.c
@@ -17,18 +17,19 @@ static const struct { const char *key; type value; } name##_table[] =
 
 #define BINARY_SEARCH_PARSER(name, type) \
 bool parse_##name(char *s, type *v) { \
-   int left = 0, right = sizeof(name##_table)/sizeof(name##_table[0]) - 1; \
-   while (left <= right) { \
-       int mid = (left + right) >> 1; \
-       int cmp = strcmp(s, name##_table[mid].key); \
-       if (cmp == 0) { \
-           *v = name##_table[mid].value; \
-           return true; \
-       } \
-       left = cmp > 0 ? mid + 1 : left; \
-       right = cmp < 0 ? mid - 1 : right; \
-   } \
-   return false; \
+    if (!s || !v) return false; \
+    int left = 0, right = (sizeof(name##_table)/sizeof(name##_table[0])) - 1; \
+    while (left <= right) { \
+        int mid = left + ((right - left) >> 1); /* Avoid overflow */ \
+        int cmp = strcmp(s, name##_table[mid].key); \
+        if (cmp == 0) { \
+            *v = name##_table[mid].value; \
+            return true; \
+        } \
+        if (cmp > 0) left = mid + 1; \
+        else right = mid - 1; \
+    } \
+    return false; \
 }
 
 // sorted lookup tables
